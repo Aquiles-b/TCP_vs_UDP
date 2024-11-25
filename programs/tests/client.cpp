66ffe9031ef826fce39b249f6e1eaff1eb7a51ab
marzelop <maagyo.teo23@gmail.com>
2024-11-25 14:03:53 -0300

    Update client instanciation

--- programs/tests/client.cpp
+++ programs/tests/client.cpp
@@ -1,15 +1,23 @@
 #include "../include/tcp_client.hpp"
+#include <memory>
 
 using namespace tcp_vs_udp;
 
 int main(int argc, char **argv) {
-	if (argc < 4) {
-		std::cerr << "Correct usage: ./client <server_ipv4_addr> <server_port> <fname>\n";
+	if (argc < 5) {
+		std::cerr << "Correct usage: ./client <server_ipv4_addr> <server_port> <fname> <protocol>\n";
 		exit(1);
 	}
-	TCPClient c = TCPClient(argv[1], argv[2]);
-	c.connectToServer();
-	c.download(argv[3]);
+	std::unique_ptr<BasicClient> c;
+	if (strcmp("tcp", argv[4]) == 0)
+		c = std::make_unique<TCPClient>(argv[1], argv[2]);
+	else {
+		std::cerr << "Invalid protocol type.\n";
+		exit(1);
+	}
+
+	c->connectToServer();
+	c->download(argv[3]);
 
 	return 0;
 }
